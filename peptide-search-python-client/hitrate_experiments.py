a9a9e8c7a523e7cd131b1bda1302ea219d401fcc
Sahil <arolihas@gmail.com>
2020-11-21 21:37:18 +0000

    hitrate subseq1 fix

--- peptide-search-python-client/hitrate_experiments.py
+++ peptide-search-python-client/hitrate_experiments.py
@@ -1,14 +1,14 @@
 import hitrate_calculator
 from hitrate_calculator import parse_subseqs
 
-PATH_DIR = '../lstm_attn/lstm_linear_model/subsequences'
+PATH_DIR = '../lstm_attn/lstm_linear_model/subsequences/'
 
 # lstm2_attn = 'above_top2std_subsequences_testData_avgLens5.csv'
 # lstm2_rand = 'random_above_top2std_subsequences_testData_avgLens5.csv'
 # lstm2_perm = 'permute_above_top2std_subsequences_testData_avgLens5.csv'
-lstm1_attn = 'above_top1std_subsequences_testData_avgLens5.csv'
-lstm1_rand ='random_above_top1std_subsequences_testData_avgLens5.csv'
-lstm1_perm ='permute_above_top1std_subsequences_testData_avgLens5.csv'
+lstm1_attn = 'above_top1std_subsequences_testData_avgLens3.csv'
+lstm1_rand ='random_above_top1std_subsequences_testData_avgLens3.csv'
+lstm1_perm ='permute_above_top1std_subsequences_testData_avgLens3.csv'
 
 print('model ', parse_subseqs(PATH_DIR, lstm1_attn))
 print('permuted ', parse_subseqs(PATH_DIR, lstm1_perm, use_k_set=True))
