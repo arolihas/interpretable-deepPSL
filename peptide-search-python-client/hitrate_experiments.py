f4f08de42591c0e20f5027ada794e344960ec16a
Sahil Arora <sahil.arora@ibm.com>
2020-11-21 16:24:14 -0500

    hitrate typo

--- peptide-search-python-client/hitrate_experiments.py
+++ peptide-search-python-client/hitrate_experiments.py
@@ -8,7 +8,7 @@ PATH_DIR = '../lstm_attn/lstm_linear_model/subsequences'
 # lstm2_perm = 'permute_above_top2std_subsequences_testData_avgLens5.csv'
 lstm1_attn = 'above_top1std_subsequences_testData_avgLens5.csv'
 lstm1_rand ='random_above_top1std_subsequences_testData_avgLens5.csv'
-lstm1_perm ='permute_above_top1std_subsequences_testData_avgLens5.csv
+lstm1_perm ='permute_above_top1std_subsequences_testData_avgLens5.csv'
 
 print('model ', parse_subseqs(PATH_DIR, lstm1_attn))
 print('permuted ', parse_subseqs(PATH_DIR, lstm1_perm, use_k_set=True))
